5216a7538fbb265dae45ebead5c06bdbca8c3e4f
Achal Dave <achalddave@live.com>
2013-07-31 22:17:01 -0700

    artifact intensity still felt low

--- src/com/example/basiccameraapp/BlurArtifactInducer.java
+++ src/com/example/basiccameraapp/BlurArtifactInducer.java
@@ -1,16 +1,21 @@
 package com.example.basiccameraapp;
 
 import android.graphics.Bitmap;
+import android.util.Log;
 
 public class BlurArtifactInducer extends ArtifactInducer {
+	private static String TAG = BlurArtifactInducer.class.getName();
+
 	@Override
 	public Bitmap induceArtifacts(Bitmap image, float artifactIntensity) {
-		//			  offset	     map artifactIntensity to be bwn 0.5, 1
-		float scale = 1.001f - (artifactIntensity / 2 + 0.5f);
+		//			  offset	     map artifactIntensity to be bwn 0.8, 1
+		float scale = 1.001f - (artifactIntensity*0.2f + 0.8f);
 		int scaledHeight = (int) (image.getHeight() * scale);
 		scaledHeight = scaledHeight == 0 ? 1 : scaledHeight;
 		int scaledWidth = (int) (image.getWidth() * scale);
 		scaledWidth = scaledWidth == 0 ? 1 : scaledWidth;
+		Log.d(TAG, "H: " + image.getHeight() + ", W: " + image.getWidth());
+		Log.d(TAG, "Scaled H: " + scaledHeight + ", W: " + scaledWidth);
 		return Bitmap.createScaledBitmap(
 				Bitmap.createScaledBitmap(image, scaledHeight, scaledWidth, true),
 				image.getWidth(), image.getHeight(), true);
