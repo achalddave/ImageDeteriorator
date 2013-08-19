728363a45dc8b4eee6cffe4a75463c91ebea6a74
Achal Dave <achalddave@live.com>
2013-08-19 23:07:57 +0100

    Better logic for Gallery refreshing/loading
    This is the 1am post-shower code that fixed almost all the bugs I had with
    respect to async loading and refreshing. yay :)

--- src/com/example/basiccameraapp/GalleryAdapter.java
+++ src/com/example/basiccameraapp/GalleryAdapter.java
@@ -1,11 +1,21 @@
 package com.example.basiccameraapp;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
-import android.util.Log;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
@@ -15,31 +25,101 @@ import com.example.basiccameraapp.application.BasicCameraApplication;
 
 public class GalleryAdapter extends BaseAdapter {
 	static String TAG = GalleryAdapter.class.getName();
+	static int refreshRate = 500;
+	static int initialDelay = 2000;
+
+	Activity mActivity;
+	BasicCameraApplication mApp;
+	SharedPreferences mPrefs;
+
+	GridView mGallery;
 	ImageCache mImageCache;
-	Context mContext;
 	String[] mImagePaths;
 	View.OnClickListener mImageViewListener;
-	BasicCameraApplication mApp;
-	GridView mGallery;
+	HashMap<String, ImageLoader> mImageLoadersByPath;
 
-	public GalleryAdapter(Context context, GridView gallery, String[] imagePaths, ImageCache imageCache) {
-		mContext = context;
+	ArtifactInducer mArtifactInducer;
+	Timer mThumbnailsUpdateTimer;
+	int mDisplayHeight, mDisplayWidth;
+	HashMap<String, Integer> mImageViewTimesCache;
+
+	public GalleryAdapter(Activity activity, GridView gallery, String[] imagePaths, ImageCache imageCache) {
+		mActivity = activity;
 		mGallery = gallery;
 		mImagePaths = imagePaths;
 		mImageCache = imageCache;
+		mImageLoadersByPath = new HashMap<String, ImageLoader>();
 		mGallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> gallery, View view, int pos,
 					long id) {
 				String path = (String) view.getTag();
 				if (path != null) {
-					Log.i(TAG, "Opening image: " + path);
-					GalleryActivity.viewImage(GalleryAdapter.this.mContext, path);
-				} else {
-					Log.e(TAG, "No path associated with image at position " + pos + "!");
+					GalleryActivity.viewImage(GalleryAdapter.this.mActivity, path);
 				}
 			}
 		});
+
+		mImageViewTimesCache = new HashMap<String, Integer>();
+		mArtifactInducer = new BlurArtifactInducer();
+
+		mPrefs = activity.getSharedPreferences(MainActivity.DEFAULT_PREFS_NAME, Context.MODE_PRIVATE);
+		if (! mPrefs.contains(MainActivity.PREFS_KEY_SCREEN_HEIGHT)) {
+			DisplayMetrics metrics = mActivity.getResources().getDisplayMetrics();
+			Editor prefsEditor = mPrefs.edit();
+			mDisplayHeight = metrics.heightPixels;
+			mDisplayWidth  = metrics.widthPixels;
+			prefsEditor.putInt(MainActivity.PREFS_KEY_SCREEN_HEIGHT, metrics.heightPixels);
+			prefsEditor.putInt(MainActivity.PREFS_KEY_SCREEN_WIDTH, metrics.widthPixels);
+			prefsEditor.commit();
+		} else {
+			mDisplayHeight = mPrefs.getInt(MainActivity.PREFS_KEY_SCREEN_HEIGHT, -1);
+			mDisplayWidth = mPrefs.getInt(MainActivity.PREFS_KEY_SCREEN_WIDTH, -1);
+		}
+	}
+
+	public void startRefresh() {
+		populateImageViewTimesCache();
+		mThumbnailsUpdateTimer = new Timer();
+		mThumbnailsUpdateTimer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				mActivity.runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						updateImageTimesInCache(refreshRate);
+						notifyDataSetChanged();
+					}
+				});
+			}
+		}, initialDelay, refreshRate);
+	}
+
+	public void stopRefresh() {
+		commitTimeUpdatesToCache();
+	}
+
+	public void populateImageViewTimesCache() {
+		mImageViewTimesCache.clear();
+		for (String path : mImagePaths) {
+			mImageViewTimesCache.put(path, mPrefs.getInt(path, 0));
+		}
+	}
+
+	public void updateImageTimesInCache(int updateMs) {
+		for (Map.Entry<String, Integer> entry : mImageViewTimesCache.entrySet()) {
+			String path = entry.getKey();
+			int currentTime = mImageViewTimesCache.get(path);
+			mImageViewTimesCache.put(path, Math.min(MainActivity.sTimeToImageDeath, currentTime + updateMs));
+		}
+	}
+
+	public void commitTimeUpdatesToCache() {
+		Editor prefsEditor = mPrefs.edit();
+		for (Map.Entry<String, Integer> image : mImageViewTimesCache.entrySet()) {
+			prefsEditor.putInt(image.getKey(), image.getValue().intValue());
+		}
+		prefsEditor.commit();
 	}
 
 	@Override
@@ -57,75 +137,56 @@ public class GalleryAdapter extends BaseAdapter {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ImageView imageView;
+		String path = (String) getItem(position);
 		if (convertView == null) {
-			imageView = new ImageView(mContext);
+			imageView = new ImageView(mActivity);
+			imageView.setTag(path);
 			imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
 			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 			imageView.setPadding(0,10,0,10);
 		} else {
 			imageView = (ImageView) convertView;
+			imageView.setTag(path);
 		}
 
-		String path = (String) getItem(position);
 		Bitmap cached = mImageCache.get(path);
 		if (cached == null) {
-			imageView.setImageResource(android.R.drawable.ic_menu_help);
-			ImageLoaderData img = new ImageLoaderData(imageView, this, position);
-			new ImageLoader().execute(img);
+			if (! mImageLoadersByPath.containsKey(path)) {
+				ImageLoader loader = new ImageLoader();
+				loader.execute(path);
+				mImageLoadersByPath.put(path, loader);
+			}
 		} else {
-			imageView.setTag(path);
-			imageView.setImageBitmap(cached);
+			setArtifactInducedBitmap(imageView, cached);
 		}
 
 		return imageView;
 	}
-
+	
 	@Override
 	public int getCount() {
 		return mImagePaths.length;
 	}
 
-	class ImageLoader extends AsyncTask<ImageLoaderData, Void, ImageLoaderData[]> {
-		@Override
-		protected ImageLoaderData[] doInBackground(ImageLoaderData... images) {
-			for (ImageLoaderData image : images) {
-				Bitmap bmp = GalleryActivity.decodeSampledBitmapFromPath(
-						image.mAdapter.mContext, image.getPath(), 100, 100);
-
-				image.mBmp = bmp;
-			}
-			return images;
-		}
-
-		@Override
-		protected void onPostExecute(ImageLoaderData[] images) {
-			for (final ImageLoaderData image : images) {
-				ImageView imageView = image.mImageView;
-				final String path = image.getPath();
-				if (image.mBmp != null) {
-					image.mAdapter.mImageCache.put(path, image.mBmp);
-					imageView.setImageBitmap(image.mBmp);
-					imageView.setTag(path);
-				} else {
-					imageView.setImageResource(android.R.drawable.ic_menu_help);
-				}
-			}
-		}
+	public void setArtifactInducedBitmap(ImageView iv, Bitmap bmp) {
+		int timeViewed = mImageViewTimesCache.get(iv.getTag()).intValue();
+		bmp = mArtifactInducer.induceArtifacts(bmp, mDisplayWidth, mDisplayHeight, (float) timeViewed/MainActivity.sTimeToImageDeath);
+		iv.setImageBitmap(bmp);
 	}
 
-	class ImageLoaderData {
-		public ImageView mImageView;
-		public GalleryAdapter mAdapter;
-		public int mPosition;
-		public Bitmap mBmp;
-		public ImageLoaderData(ImageView imageView, GalleryAdapter adapter, int position) {
-			mImageView = imageView;
-			mAdapter = adapter;
-			mPosition = position;
+	class ImageLoader extends AsyncTask<String, Void, Void> {
+		@Override
+		protected Void doInBackground(String... paths) {
+			String path = paths[0];
+			Bitmap bmp = GalleryActivity.decodeSampledBitmapFromPath(
+					GalleryAdapter.this.mActivity, path, 100, 100);
+			GalleryAdapter.this.mImageCache.put(path, bmp);
+			return null;
 		}
-
-		public String getPath() {
-			return (String) mAdapter.getItem(mPosition);
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			GalleryAdapter.this.notifyDataSetChanged();
 		}
 	}
 }
